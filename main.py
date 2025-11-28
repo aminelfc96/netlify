5c1b07c34ec82319379bc4362951acbeab22d06e
Amine <64668497+aminelfc96@users.noreply.github.com>
2025-11-28 18:25:33 +0100

    Update Flask app to include JSON response

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return f"<h1>Task 2 - Deployed in render web service</h1>"
+    return f"<title>Mohamed Amine Taieb</title><h1>Task 2 - Deployed in render web service</h1>"
 
 
 @app.route('/hello')
 def submit():
-    return f"<p>Hello</p>"
+    return jsonify(message="Hello")
 
 if __name__ == '__main__':
 
     app.run(host="0.0.0.0", port=10000)
+
